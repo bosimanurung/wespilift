28c30390951fb0c37b66d1c3e239d220135f11bc
Bosi Manurung <94754059+bosimanurung@users.noreply.github.com>
2024-09-27 15:18:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import pandasql as ps
+import altair as alt
 from PIL import Image
 
 st.set_page_config(layout="wide")
@@ -10,18 +12,21 @@ st.sidebar.image(image)
 
 # -- page setup --
 my_calculations = st.Page(
-    page = "views/my_calc.py",
+    #page = "views/my_calc.py",
+    page = "my_calc.py",
     title = "My Calculations",
     icon = ":material/account_circle:",
     default = True,
 )
 input_page = st.Page(
-    page = "views/data_input.py",
+    #page = "views/data_input.py",
+    page = "data_input.py",
     title = "Data Input",
     icon = ":material/bar_chart:",
 )
 ipr_curve = st.Page(
-    page = "views/ipr_curve.py",
+    #page = "views/ipr_curve.py",
+    page = "ipr_curve.py",
     title = "Calculation & IPR Curve",
     icon = ":material/bar_chart:",
 )
